f28f3c3f63fa024e7a03e4370a19468aeaebee8e
DatLQ163 <lequocdat163@gmail.com>
2022-11-04 16:51:14 +0700

    update1

--- src/main/java/PageObjects/Timetable.java
+++ src/main/java/PageObjects/Timetable.java
@@ -2,11 +2,30 @@ package PageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Timetable {
     WebDriver driver = new ChromeDriver();
     // Locators
-    private By rowTicketInfor = By.cssSelector(".MyTable tr:not(.TableSmallHeader)");
-    private By lkCheckPrice = By.
+    private By rowTicketInfo = By.cssSelector(".MyTable tr:not(.TableSmallHeader)");
+    private By lkCheckPrice = By.xpath("//a[.= 'check price']");
+    private By lkBookTicket = By.xpath("//a[.= 'book ticket']");
+
+    // Elements
+    private WebElement getRowTicketInfo(){
+        return driver.findElement(rowTicketInfo);
+    }
+    private WebElement getLkCheckPrice(){
+        return driver.findElement(lkCheckPrice);
+    }
+    private WebElement getLkBookTicket(){
+        return driver.findElement(lkBookTicket);
+    }
+
+    // Methods
+    public void checkLink(){
+//        clickOnCheckPrice();
+//        clickOnBookTicket();
+    }
 }
